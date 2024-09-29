53a3200d8f5e705383b45ab2456f2dc8e43b73b3
Tejas-Gojariya <tejasgojariya036@gmail.com>
2024-09-29 12:50:36 +0530

    apply framer-motion in contact page

---
+++ Portfolio/src/Components/ContactPage/ContactAnimation.jsx
@@ -0,0 +1,34 @@
+import { motion as m } from "framer-motion";
+import React from "react";
+
+const ContactAnimation = () => {
+  return (
+    <div className="absolute top-0 left-0 w-full h-full overflow-hidden">
+      {/* First animation layer */}
+      <m.div
+        initial={{ transform: "translateY(0%)" }}
+        animate={{ transform: "translateY(-100%)" }}
+        transition={{
+          duration: 0.5,
+          delay: 0.1,
+          ease: "easeIn",
+        }}
+        className="absolute top-0 left-0 w-full h-full bg-[#ece7e1] z-50"
+      ></m.div>
+
+      {/* Second animation layer */}
+      <m.div
+        initial={{ transform: "translateY(-30%)" }}
+        animate={{ transform: "translateY(-100%)" }}
+        transition={{
+          duration: 1,
+          delay: 0.1,
+          ease: "ease",
+        }}
+        className="absolute top-0 left-0 w-full h-full bg-[#ece7e1] z-50"
+      ></m.div>
+    </div>
+  );
+};
+
+export default ContactAnimation;
--- Portfolio/src/routes/Contact.jsx
+++ Portfolio/src/routes/Contact.jsx
@@ -1,49 +1,12 @@
 import React from "react";
+import ContactAnimation from "../Components/ContactPage/ContactAnimation";
+import ContactPage from "../Components/ContactPage/ContactPage";
 
 const contact = () => {
   return (
-    <div className="bg-[#1a1818] text-[#ece7e1] w-full h-screen flex justify-start items-start md:items-center pt-28 md:pt-0 px-8 md:px-20">
-      {/* Main Content Wrapper */}
-      <div className="w-full xl1100:w-[50%] h-auto xl1100:h-full flex justify-start md:justify-center xl1100:justify-end items-end xl1100:items-center">
-        {/* Text Section */}
-        <div className="w-full sm350:w-3/4 h-auto flex flex-col justify-center items-start gap-4">
-          <div className="overflow-hidden sm:-ml-1 mb-8 sm500:mb-4 xl1100:-mb-[10%]">
-            <div className="text-[19vw] sm500:text-[21vw] md:text-[16vw] xl1100:text-[14vw] Avegas-Royale-Regular">
-              Hello.
-            </div>
-          </div>
-
-          {/* Contact Information */}
-          <div className="overflow-hidden xl1100:pt-10 -mt-10 relative z-30 bg-[#1a1818]">
-            <div className="w-full sm:w-[85%] flex flex-col gap-4">
-              {/* Introduction Text */}
-              <div className="text-[12px] leading-5 sm500:text-[14px] sm500:leading-7">
-                Need a Front-End Developer who is eager to bring creativity and
-                dedication to a dynamic organisation? Get in touch.
-              </div>
-
-              {/* Contact Details */}
-              <div className="text-[12px] leading-5 sm500:text-[14px] sm500:leading-7 flex flex-col gap-2">
-                <div className="flex gap-2">
-                  <span>Email:</span>
-                  <div
-                    to="mailto:tejasgojariya036@gmail.com"
-                    className="underline underline-offset-4 underline-[#ece7e194] hover:opacity-50"
-                  >
-                    tejasgojariya036@gmail.com
-                  </div>
-                </div>
-                <div className="flex gap-2">
-                  <span>On the Internet:</span>
-                  <div to="" target="_blank">
-                    LinkedIn
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div>
+      <ContactAnimation />
+      <ContactPage />
     </div>
   );
 };
