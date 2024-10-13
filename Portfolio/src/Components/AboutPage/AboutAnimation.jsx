7a9e125269e7b3053ebd0cdd58cd4ac945099965
Tejas-Gojariya <tejasgojariya036@gmail.com>
2024-10-13 12:26:36 +0530

    update information in about page

--- Portfolio/src/Components/AboutPage/AboutAnimation.jsx
+++ Portfolio/src/Components/AboutPage/AboutAnimation.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { motion as m } from "framer-motion";
 
 const AboutAnimation = () => {
   return (
-    <div>AboutAnimation</div>
-  )
-}
+    <div className="absolute top-0 left-0 w-full h-full overflow-hidden">
+      <m.div
+        initial={{ transform: "translateY(0%)" }}
+        animate={{ transform: "translateY(-101%)" }}
+        transition={{
+          duration: 0.5,
+          delay: 0,
+          ease: "easeIn",
+        }}
+        className="absolute top-0 left-0 w-full h-full bg-[#1a1818] z-50"
+      ></m.div>
+      <m.div
+        initial={{ transform: "translateY(30%)" }}
+        animate={{ transform: "translateY(-101%)" }}
+        transition={{
+          duration: 1,
+          delay: 0,
+          ease: "ease",
+        }}
+        className="absolute top-0 left-0 w-full h-full bg-[#1a1818] z-50"
+      ></m.div>
+    </div>
+  );
+};
 
-export default AboutAnimation
+export default AboutAnimation;
--- Portfolio/src/Components/AboutPage/AboutPage.jsx
+++ Portfolio/src/Components/AboutPage/AboutPage.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { motion as m } from "framer-motion";
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
-  )
-}
+    <div className="relative w-full h-auto flex justify-start items-start md:items-center pt-28 md:pt-0 px-8 md:px-20">
+      <div className="w-full xl1100:w-[60%] h-auto flex flex-col justify-start md:justify-start items-start md:ml-20 xl1100:ml-60 pb-20 md:pt-20">
+        <div className="overflow-hidden w-full xl1100:-mb-10">
+          <m.div
+            initial={{ transform: "translateY(100%)" }}
+            animate={{ transform: "translateY(0%)" }}
+            transition={{
+              duration: 0.5,
+              delay: 0.75,
+            }}
+            className="Avegas-Royale-Regular text-[20vw] sm500:text-[22vw] md:text-[15vw]"
+          >
+            About
+          </m.div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default AboutPage
+export default AboutPage;
