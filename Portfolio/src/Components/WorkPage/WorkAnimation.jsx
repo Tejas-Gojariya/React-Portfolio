a0dbe34e82a1896cc5748fc78fc392c31ad70482
Tejas-Gojariya <tejasgojariya036@gmail.com>
2024-10-16 22:52:30 +0530

    add animation in work page and work title

--- Portfolio/src/Components/WorkPage/WorkAnimation.jsx
+++ Portfolio/src/Components/WorkPage/WorkAnimation.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { motion as m } from "framer-motion";
+import React from "react";
 
 const WorkAnimation = () => {
   return (
-    <div>WorkAnimation</div>
-  )
-}
+    <div className="absolute top-0 left-0 w-full h-full overflow-hidden">
+      <m.div
+        initial={{ transform: "translateY(0%)" }}
+        animate={{ transform: "translateY(-100%)" }}
+        transition={{
+          duration: 0.5,
+          delay: 0.1,
+          ease: "easeIn",
+        }}
+        className="absolute top-0 left-0 w-full h-full bg-[#ece7e1] z-50"
+      ></m.div>
 
-export default WorkAnimation
+      <m.div
+        initial={{ transform: "translateY(-30%)" }}
+        animate={{ transform: "translateY(-100%)" }}
+        transition={{
+          duration: 1,
+          delay: 0.1,
+          ease: "ease",
+        }}
+        className="absolute top-0 left-0 w-full h-full bg-[#ece7e1] z-50"
+      ></m.div>
+    </div>
+  );
+};
+
+export default WorkAnimation;
--- Portfolio/src/Components/WorkPage/WorkPage.jsx
+++ Portfolio/src/Components/WorkPage/WorkPage.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import { motion as m } from "framer-motion";
 
 const WorkPage = () => {
   return (
-    <div>WorkPage</div>
-  )
-}
+    <div className="bg-[#1a1818] text-[#ece7e1] w-full h-full xl1100:h-screen flex flex-col xl1100:flex-row pb-28 xl1100:pb-0 pt-28 md:pt-60 xl1100:pt-0 justify-start xl1100:justify-between items-center gap-8 sm500:gap-12 md:gap-12 xl1100:gap-20 px-8 md:px-20">
+      {/* Left section with text */}
+      <div className="w-full xl1100:w-[35%] h-auto xl1100:h-full flex justify-start md:justify-center xl1100:justify-end items-end xl1100:items-center">
+        <div className="w-full sm350:w-3/4 h-auto flex flex-col justify-center items-start gap-4 mt-0 xl1100:mt-[50%]">
+          {/* "WORK" title animation */}
+          <div className="overflow-hidden mb-2">
+            <m.div
+              initial={{ transform: "translateY(100%)" }}
+              animate={{ transform: "translateY(0%)" }}
+              transition={{
+                duration: 0.5,
+                delay: 0.5,
+              }}
+              className="uppercase font-medium tracking-wider text-sm sm500:text-base"
+            >
+              WORK
+            </m.div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default WorkPage
+export default WorkPage;
